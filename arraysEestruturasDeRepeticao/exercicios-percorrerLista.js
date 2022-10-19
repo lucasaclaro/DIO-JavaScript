01c202d284c9eab3a38fd55a1f16dfd4f1533d71
Lucas Claro <lucas-claro@hotmail.com>
2022-10-19 08:14:44 -0300

    exercícios de arrays e estruturas de repetição

---
+++ arraysEestruturasDeRepeticao/exercicios-percorrerLista.js
@@ -0,0 +1,14 @@
+/*
+
+Criar uma lista para percorrer os números pares
+
+*/
+
+lista = [10, 20, 30, 35, 50, 55, 80];
+
+for (i = 0; i < lista.length; i++) {
+    if (lista[i] % 2 === 0) {
+        console.log(lista[i])
+    } 
+
+}
---
+++ arraysEestruturasDeRepeticao/exercicios-tabuada.js
@@ -0,0 +1,5 @@
+const numero = 5;
+
+for (i = 0; i <= 10; i++) {
+    console.log(`${i} x ${numero} = ${i * numero}`)
+}
